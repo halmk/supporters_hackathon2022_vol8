99bd3e9cd0cea2159822c56c59a332451f69441d
halmk <sakurai.apple1@gmail.com>
2022-11-22 01:53:58 +0000

    #14 Create: プロジェクトインデックスページ（仮）を作成

---
+++ frontend/pages/projects/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <div class="ma-1 pa-3 grey lighten-4">
+        <span class="text-h4"> プロジェクトを作成する </span>
+        <div class="mt-1">
+          <ProjectCreateForm />
+        </div>
+      </div>
+    </v-col>
+    <v-col cols="12">
+      <div class="ma-1 pa-3 grey lighten-4">
+        <span class="text-h4"> プロジェクトリスト </span>
+        <div class="mt-2">
+          <ProjectList />
+        </div>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'ProjectIndexPage',
+}
+</script>
