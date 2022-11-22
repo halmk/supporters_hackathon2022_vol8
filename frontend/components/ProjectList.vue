be55986f41fff884ab20d03bcbd3ba8a6d7411fb
halmk <sakurai.apple1@gmail.com>
2022-11-22 01:42:51 +0000

    #14 #9 Create: プロジェクトリスト（仮）を作成

---
+++ frontend/components/ProjectList.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card tile>
+    <v-list-item
+      v-for="(project, i) in projects"
+      :key="i"
+      :to="project.name"
+      append
+      router
+      exact
+    >
+      <v-list-item-content>
+        <v-list-item-title>{{ project.name }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'ProjectList',
+  props: {
+    // projects: Array,
+  },
+  data: () => ({
+    projects: [
+      {
+        name: 'project1',
+      },
+      {
+        name: 'project2',
+      },
+    ],
+  }),
+  computed: {},
+  methods: {},
+}
+</script>
