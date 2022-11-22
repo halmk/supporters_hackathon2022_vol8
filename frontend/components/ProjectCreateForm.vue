c2408514a7cf9a0d630e9cb2980f888ff4d243bb
halmk <sakurai.apple1@gmail.com>
2022-11-22 01:40:22 +0000

    #14 #8 Create: プロジェクト作成フォーム（仮）を作成

---
+++ frontend/components/ProjectCreateForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            v-model="projectName"
+            :rules="nameRules"
+            label="Project name"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-btn
+            :disabled="!valid"
+            color="primary"
+            class="mr-4"
+            @click="submit"
+          >
+            Create
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+<script>
+export default {
+  name: 'ProjectCreateForm',
+  data: () => ({
+    projectName: '',
+    valid: false,
+    nameRules: [(v) => !!v || 'required'],
+  }),
+  computed: {},
+  methods: {
+    submit() {},
+  },
+}
+</script>
