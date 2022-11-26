290c846e5867dbfded988fedb9970102babbec33
halmk <elle2206@i.softbank.jp>
2022-11-26 07:55:03 +0000

    #30 Create: カテゴリ作成フォームを作成

---
+++ frontend/components/CategoryCreateForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form v-model="valid">
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="categoryName"
+          :rules="nameRules"
+          label="Category Name"
+          required
+          dense
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="2">
+        <v-btn :disabled="!valid" color="primary" class="mr-4" @click="submit">
+          Add
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+<script>
+export default {
+  name: 'CategoryCreateForm',
+  data: () => ({
+    categoryName: '',
+    valid: false,
+    nameRules: [(v) => !!v || 'required'],
+  }),
+  computed: {},
+  methods: {
+    submit() {
+      this.$emit('submit', this.categoryName)
+    },
+  },
+}
+</script>
