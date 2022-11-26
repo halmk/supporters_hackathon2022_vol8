15c885a8d41fd150a17354c8f2789ddac1488177
halmk <elle2206@i.softbank.jp>
2022-11-26 07:52:50 +0000

    #30 #12 Add: loadingをpropsに追加

--- frontend/components/BookmarkSubmissionForm.vue
+++ frontend/components/BookmarkSubmissionForm.vue
@@ -1,19 +1,36 @@
 <template>
   <v-form v-model="valid">
-    <v-text-field
-      v-model="bookmarkURL"
-      :rules="nameRules"
-      label="bookmark url"
-      required
-    ></v-text-field>
-    <v-btn :disabled="!valid" color="primary" class="mr-4" @click="submit">
-      Submit
-    </v-btn>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="bookmarkURL"
+          :rules="nameRules"
+          label="Bookmark URL"
+          required
+          dense
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-btn
+          :disabled="!valid"
+          color="primary"
+          class="mr-4"
+          :loading="loading"
+          @click="submit"
+        >
+          Submit
+        </v-btn>
+      </v-col>
+    </v-row>
   </v-form>
 </template>
+
 <script>
 export default {
   name: 'BookmarkSubmissionForm',
+  props: {
+    loading: Boolean,
+  },
   data: () => ({
     bookmarkURL: '',
     valid: false,
