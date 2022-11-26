052d93e8908da57d38e54666ed4b13ce0455aa2c
halmk <elle2206@i.softbank.jp>
2022-11-26 07:55:35 +0000

    #30 Create: カテゴリ選択ボタンを作成

---
+++ frontend/components/CategorySelectButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-overflow-btn
+    :items="categories"
+    label="Select Category"
+    hide-details
+    dense
+    class="pa-0"
+    @input="input"
+  ></v-overflow-btn>
+</template>
+
+<script>
+export default {
+  name: 'CategorySelectButton',
+  props: {
+    categories: Array,
+  },
+  data: () => ({}),
+  computed: {},
+  methods: {
+    input(category) {
+      this.$emit('input', category)
+    },
+  },
+}
+</script>
