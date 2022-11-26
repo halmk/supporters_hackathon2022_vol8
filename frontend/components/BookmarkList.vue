2fe6de3295e0c456c3c1c1ffb311c658706554c1
halmk <elle2206@i.softbank.jp>
2022-11-26 07:51:16 +0000

    #30 #16 Mod: bookmarksをpropsに移動

--- frontend/components/BookmarkList.vue
+++ frontend/components/BookmarkList.vue
@@ -9,32 +9,9 @@
 export default {
   name: 'BookmarkList',
   props: {
-    // bookmarks: Array,
+    bookmarks: Array,
   },
-  data: () => ({
-    bookmarks: [
-      {
-        url: 'http://example.com',
-        thumbnailURL: 'http://example.com/thumbnail.png',
-        title: 'example.com',
-        content: 'abcdefg',
-        cratedAt: '2022-11-22T9:00:00',
-        project: 'project1',
-        category: 'frontend',
-        sender_uuid: 'abc',
-      },
-      {
-        url: 'http://example.com',
-        thumbnailURL: 'http://example.com/thumbnail.png',
-        title: 'example.com',
-        content: 'abcdefg',
-        cratedAt: '2022-11-22T9:00:00',
-        project: 'project1',
-        category: 'frontend',
-        sender_uuid: 'xyz',
-      },
-    ],
-  }),
+  data: () => ({}),
   computed: {},
   methods: {},
 }
