bb39a12f690a66a45d32776c90f3553e226d2c56
halmk <elle2206@i.softbank.jp>
2022-11-27 03:09:42 +0000

    #11 Add: 各コメントのユーザ名を取得

--- frontend/components/CommentDialog.vue
+++ frontend/components/CommentDialog.vue
@@ -1,13 +1,13 @@
 <template>
   <v-dialog :value="dialog" max-width="500" @click:outside.stop="clickOutside">
     <v-card tile>
-      <v-card-title class="text-h5">
+      <v-card-title class="text-h7">
         {{ title }}
       </v-card-title>
-      <v-list-item v-for="(comment, i) in comments" :key="i">
+      <v-list-item v-for="(comment, i) in showComments" :key="i">
         <v-list-item-content>
           <v-list-item-title>{{ comment.username }}</v-list-item-title>
-          <v-list-item-subtitle>{{ comment.content }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ comment.comment }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-card-actions>
@@ -21,27 +21,56 @@
 export default {
   name: 'CommentDialog',
   props: {
-    // title: String,
-    // comments: Array,
+    title: String,
+    comments: Array,
     dialog: Boolean,
   },
   data: () => ({
-    title: 'title1',
-    comments: [
-      {
-        username: 'A',
-        content: 'abcdefg',
-      },
-      {
-        username: 'B',
-        content: 'hijklmn',
-      },
-    ],
+    showComments: [],
   }),
   computed: {},
+  watch: {
+    async comments() {
+      this.showComments = this.comments
+      if (!this.comments) {
+        return []
+      }
+      const users = await this.getUsers()
+      console.log(users)
+      this.showComments.forEach((comment, index) => {
+        for (const user of users) {
+          if (user.uuid === comment.sender_uuid) {
+            const newComment = comment
+            newComment.username = user.name
+            this.showComments.splice(index, 1, newComment)
+          }
+        }
+      })
+    },
+  },
   methods: {
     clickOutside() {
       this.$emit('clickOutside')
+    },
+
+    async getUsers() {
+      try {
+        const users = []
+        for (const comment of this.comments) {
+          // UUIDと一致するユーザをfirestoreから取得
+          const db = this.$fire.firestore
+          const snapshot = await db
+            .collection('users')
+            .doc(comment.sender_uuid)
+            .get()
+          const user = snapshot.data()
+          console.log(user)
+          users.push({ uuid: comment.sender_uuid, name: user.name })
+        }
+        return users
+      } catch (error) {
+        console.log(error)
+      }
     },
   },
 }
